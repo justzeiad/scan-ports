eced180405732720407bdb90d4d92c0536869206
notyuu <113541899+notyuu@users.noreply.github.com>
2023-02-24 23:49:31 +0200

    Update scan.py
    add to scan the most popular ports

--- scan.py
+++ scan.py
@@ -5,6 +5,7 @@ import argparse
 ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
 print(ascii_banner)
 print(pyfiglet.figlet_format("Made By Yuu", font = "digital" ))
+
 parser = argparse.ArgumentParser(description='Scan target URL for open ports using nmap')
 parser.add_argument('-t', '--target', type=str,nargs='?', required=True,
                     help='Target URL or IP address')
@@ -13,11 +14,15 @@ parser.add_argument('-p', '--port', type=str, nargs='?', default='-',
 parser.add_argument('-s', '--show', action='store_true',
                     help='Show the result')
 
+parser.add_argument('-m', '--mostports', action='store_true', help='Scanning Most popular ports')
+
+
 args = parser.parse_args()
 
 target = args.target
 port = args.port
 show = args.show
+most = args.mostports
 
 def Header():
     print("-"*50)
@@ -71,11 +76,28 @@ def HservByPort(p):
         serv = "Unknown"
     return serv
 
+def ScanMustPort():
+    common_ports = [21, 22, 23, 25, 53, 80, 110, 143, 443, 465, 587, 993, 995, 3306, 5432, 8080]
+    for n in common_ports:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target, n))
+        if result ==0:
+            printer = f"\033[32m[+]\033[0m \033[32m{HservByPort(n)}\033[0m Port \033[32m{n}\033[0m Is Open"
+            print(printer)
+        else:
+            printer = f"\033[31m[-]\033[0m \033[31m{HservByPort(n)}\033[0m Port \033[31m{n}\033[0m Is Close"
+            print(printer)
+        s.close()
 
 
 try:
-    if port == '-':
+    if most:
+        ScanMustPort()
+
+    elif port == '-':
         ScanAll()
+
     else:
         ScanPort(port)
         
